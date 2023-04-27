6b27f4e38a1e6ae2b4d0f5fe3ef355e4a4ac93f8
Aditya Salunke <adisalu15@gmail.com>
2023-04-28 05:11:46 +0530

    Initial Commit and added assets and started working on home screen

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ticket_booking/screen/home.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeWidget(),
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title1});
+
+  final String title1;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title1),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => const HomeWidget()));
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pixel_perfect/pixel_perfect.dart';
+
+class HomeWidget extends ConsumerWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return PixelPerfect(
+      assetPath: 'assets/pixelP.jpeg', // path to your asset image
+      scale: 2, // scale value (optional)
+      // initBottom: 20, //  default bottom distance (optional)
+      // default image offset (optional)
+      initOpacity: 0.0,
+      child: SafeArea(
+          child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: SizedBox(
+            height: 2.h,
+            child: IconButton(
+                icon: Image.asset("assets/images/netflix_logo0.png",
+                    scale: 0.5, fit: BoxFit.contain),
+                onPressed: () {},
+                iconSize: 10),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.cast),
+              iconSize: 20.sp,
+            )
+          ],
+        ),
+        body: Expanded(
+          child: Column(children: [
+            Container(
+              color: Colors.black,
+              width: 1.sw,
+              height: 0.65.sh,
+              child: Stack(
+                children: [
+                  // Image.network(
+                  //     "https://images.catchnews.com/upload/2018/06/21/surveen_118955_730x419.jpg"),
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: SizedBox(
+                          height: 0.5.sh,
+                          child: Image.asset(
+                            "assets/images/sintel.jpg",
+                            fit: BoxFit.fitHeight,
+                          ))),
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          Colors.black,
+                          Colors.black.withOpacity(0.87),
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.87),
+                          Colors.black,
+                        ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight)),
+                    height: double.infinity,
+                    width: 1.sw,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          Colors.black.withOpacity(0.35),
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.35),
+                        ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                    height: double.infinity,
+                    width: 1.sw,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 0.28.sh,
+                      width: 1.sw,
+                      color: Colors.transparent,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.w),
+                        child: Column(children: [
+                          Image.asset("assets/images/sintel_title.png"),
+                          Container(
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Text("2022"),
+                                  Text("."),
+                                  Text("Hindi"),
+                                  Text("."),
+                                  Text("Drama"),
+                                  Text("."),
+                                ]),
+                          )
+                        ]),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+        ),
+      )),
+    );
+  }
+}
